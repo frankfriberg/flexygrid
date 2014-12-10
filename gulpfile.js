20271d0e85cc0142b77917046455d8f345bf5fb2
Frank Friberg <frank.fri@me.com>
2014-12-10 15:08:20 +0100

    Added Gulp to project for compiling

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp 	= require('gulp');
+var sass 	= require('gulp-ruby-sass');
+
+
+gulp.task('scss', function () {
+    return gulp.src('scss/main.scss')
+    	// Set style to preferred: nested, compact, compressed or expanded
+        .pipe(sass({style: 'nested'}))
+        .on('error', function (err) { console.log(err.message);})
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('scss/**/*.scss', ['scss']);
+});
+
+gulp.task('default', function() {
+	gulp.start('scss');
+});
