5e4caedd5ccda435ba2d51b0cf081650deeb870d
Frank Friberg <frank.fri@me.com>
2014-12-15 15:15:57 +0100

    Moved css to dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 var gulp 	= require('gulp');
 var sass 	= require('gulp-ruby-sass');
 
-
 gulp.task('scss', function () {
     return gulp.src('scss/main.scss')
     	// Set style to preferred: nested, compact, compressed or expanded
         .pipe(sass({style: 'nested'}))
         .on('error', function (err) { console.log(err.message);})
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('dev/css'));
 });
 
 gulp.task('watch', function() {
